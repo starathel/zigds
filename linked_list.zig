4a66421984d69afe44fa0b0e1f8aa859390277e1
starathel <starathel.dev@gmail.com>
2025-04-03 11:41:57 +0300

    Linked list. Take 2

--- linked_list.zig
+++ linked_list.zig
@@ -1,115 +1,143 @@
 const std = @import("std");
+const assert = std.debug.assert;
 
 fn LinkedList(comptime T: type) type {
     return struct {
-        const Self = @This();
-
         const Node = struct {
-            data: T,
-            next: ?*Node = null,
-            prev: ?*Node = null,
+            value: T,
+            next: ?*Node,
+            prev: ?*Node,
 
-            fn create(alloc: std.mem.Allocator, data: T) !*Node {
-                const new_node = try alloc.create(Node);
-                new_node.* = .{.data = data};
-                return new_node;
+            fn init (self: *Node, val: T) void {
+                self.value = val;
+                self.next = null;
+                self.prev = null;
             }
         };
 
-        len: usize = 0,
-        head: ?*Node = null,
-        tail: ?*Node = null,
+        const Self = @This();
+
+        len: usize,
+        head: ?*Node,
+        tail: ?*Node,
 
         alloc: std.mem.Allocator,
 
         fn init(alloc: std.mem.Allocator) Self {
-            return .{ .alloc = alloc };
-        }
-
-        fn peek_back(self: Self) ?T {
-            if (self.tail == null) {
-                return null;
-            }
-            return self.tail.?.data;
+            return Self{
+                .head = null,
+                .tail = null,
+                .len = 0,
+                .alloc = alloc,
+            };
         }
 
         fn peek_front(self: Self) ?T {
-            if (self.head == null) {
-                return null;
+            if (self.head) |head| {
+                return head.value;
             }
-            return self.head.?.data;
+            return null;
         }
 
-        fn push_back(self: *Self, item: T) !void {
-            const new_node = try Node.create(self.alloc, item);
-
-            if (self.len == 0) {
-                self.head = new_node;
-                self.tail = new_node;
-            } else {
-                new_node.prev = self.tail;
-                self.tail.?.next = new_node;
-                self.tail = new_node;
+        fn peek_back(self: Self) ?T {
+            if (self.tail) |tail| {
+                return tail.value;
             }
+            return null;
+        }
+
+        fn push_back(self: *Self, val: T) !void {
+            const node = try self.alloc.create(Node);
+            node.init(val);
+
+            if (self.tail) |tail| {
+                assert(self.len != 0);
+                assert(self.head != null);
+                assert(tail.next == null);
+
+                tail.next = node;
+                node.prev = self.tail;
+                self.tail = node;
+            } else {
+                assert(self.len == 0);
+                assert(self.head == null);
+
+                self.head = node;
+                self.tail = node;
+            }
+
             self.len += 1;
         }
 
-        fn push_front(self: *Self, item: T) !void {
-            const new_node = try Node.create(self.alloc, item);
+        fn push_front(self: *Self, val: T) !void {
+            const node = try self.alloc.create(Node);
+            node.init(val);
 
-            if (self.len == 0) {
-                self.head = new_node;
-                self.tail = new_node;
+            if (self.head) |head| {
+                assert(self.len != 0);
+                assert(self.tail != null);
+                assert(head.prev == null);
+
+                head.prev = node;
+                node.next = self.head;
+                self.head = node;
             } else {
-                new_node.next = self.head;
-                self.head.?.prev = new_node;
-                self.head = new_node;
+                assert(self.len == 0);
+                assert(self.tail == null);
+
+                self.head = node;
+                self.tail = node;
             }
+
             self.len += 1;
         }
 
         fn pop_back(self: *Self) ?T {
-            if (self.len == 0) {
+            if (self.tail) |tail| {
+                const val = tail.value;
+                if (self.tail == self.head) {
+                    assert(self.len == 1);
+                    self.tail = null;
+                    self.head = null;
+                } else {
+                    tail.prev.?.next = null;
+                    self.tail = tail.prev;
+                }
+                self.alloc.destroy(tail);
+
+                self.len -= 1;
+                return val;
+            } else {
+                assert(self.head == null);
+                assert(self.len == 0);
                 return null;
             }
-
-            const ret = self.tail.?.data;
-            const prev_tail = self.tail.?;
-            if (self.len == 1) {
-                self.head = null;
-                self.tail = null;
-            }
-            else {
-                self.tail.?.prev.?.next = null;
-                self.tail = self.tail.?.prev;
-            }
-
-            self.alloc.destroy(prev_tail);
-            self.len -= 1;
-            return ret;
         }
 
         fn pop_front(self: *Self) ?T {
-            if (self.len == 0) {
+            if (self.head) |head| {
+                const val = head.value;
+                if (self.tail == self.head) {
+                    assert(self.len == 1);
+                    self.tail = null;
+                    self.head = null;
+                } else {
+                    head.next.?.prev = null;
+                    self.head = head.next;
+                }
+                self.alloc.destroy(head);
+
+                self.len -= 1;
+                return val;
+            } else {
+                assert(self.head == null);
+                assert(self.len == 0);
                 return null;
             }
-
-            const ret = self.head.?.data;
-            const prev_head = self.head.?;
-            if (self.len == 1) {
-                self.head = null;
-                self.tail = null;
-            } else {
-                self.head.?.next.?.prev = null;
-                self.head = self.head.?.next;
-            }
-
-            self.alloc.destroy(prev_head);
-            self.len -= 1;
-            return ret;
         }
     };
 }
+
 
 test LinkedList {
     var list = LinkedList(u12).init(std.testing.allocator);
@@ -139,4 +167,35 @@ test LinkedList {
     try std.testing.expectEqual(0, list.len);
     try std.testing.expectEqual(null, list.head);
     try std.testing.expectEqual(null, list.tail);
+}
+
+pub fn main() !void {
+    var allocator = std.heap.DebugAllocator(.{}).init;
+    const alloc = allocator.allocator();
+    var list = LinkedList(i32).init(alloc);
+
+    try list.push_back(1);
+    try list.push_back(2);
+    try list.push_back(3);
+    try list.push_back(4);
+    try list.push_front(0);
+    try list.push_front(-1);
+    try list.push_front(-2);
+    try list.push_front(-3);
+
+    std.debug.print("Len: {}\n", .{list.len});
+
+    var curr = list.head;
+    std.debug.print("Forward:\n", .{});
+    while (curr) |c| : (curr = c.next) {
+        std.debug.print("->{d:^4}", .{c.value});
+    }
+    std.debug.print("\n", .{});
+
+    curr = list.tail;
+    std.debug.print("Backward:\n", .{});
+    while (curr) |c| : (curr = c.prev) {
+        std.debug.print("->{d:^4}", .{c.value});
+    }
+    std.debug.print("\n", .{});
 }
